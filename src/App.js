d51f242bab6c56f443a17584f109d13cd1b9f13d
Jaeyu Jo <ehay@naver.com>
2019-06-26 03:30:38 +0900

    Apply Material UI

--- src/App.js
+++ src/App.js
@@ -1,54 +1,87 @@
 import React from 'react';
 import './App.css';
 import Coustomer from './components/Customer';
+import Paper from '@material-ui/core/Paper';
+import Table from '@material-ui/core/Table';
+import TableHead from '@material-ui/core/TableHead';
+import TableBody from '@material-ui/core/TableBody';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+import { withStyles } from '@material-ui/core/styles';
+
+const styles = theme => ({
+  root: {
+    width: '100%',
+    marginTop: theme.spacing.unit * 3,
+    overflowX: "auto"
+  },
+  table: {
+    minWidth: 1080
+  }
+})
 
 const customers = [{
-  'id' : 1,
-  'image' : 'https://placeimg.com/64/64/1',
-  'name' : '박보검',
-  'birthday' : '930303',
-  'gender' : '남자',
-  'job' : '대학생'
+  'id': 1,
+  'image': 'https://placeimg.com/64/64/1',
+  'name': '박보검',
+  'birthday': '930303',
+  'gender': '남자',
+  'job': '대학생'
 },
 {
-  'id' : 2,
-  'image' : 'https://placeimg.com/64/64/2',
-  'name' : '김태희',
-  'birthday' : '891122',
-  'gender' : '여자',
-  'job' : '디자이너'
+  'id': 2,
+  'image': 'https://placeimg.com/64/64/2',
+  'name': '김태희',
+  'birthday': '891122',
+  'gender': '여자',
+  'job': '디자이너'
 },
 {
-  'id' : 3,
-  'image' : 'https://placeimg.com/64/64/3',
-  'name' : '홍길동',
-  'birthday' : '911223',
-  'gender' : '남자',
-  'job' : '프로그래머'
+  'id': 3,
+  'image': 'https://placeimg.com/64/64/3',
+  'name': '홍길동',
+  'birthday': '911223',
+  'gender': '남자',
+  'job': '프로그래머'
 }]
 
 class App extends React.Component {
   render() {
+    const { classes } = this.props;
     return (
-      <div>
-        {
-          customers.map(c => {
-            return (
-              <Coustomer
-                key={c.id}
-                id={c.id}
-                image={c.image}
-                name={c.name}
-                birthday={c.birthday}
-                gender={c.gender}
-                job={c.job}
-              />
-            )
-          })
-        }
-      </div>
+      <Paper className={classes.root}>
+        <Table className={classes.table}>
+          <TableHead>
+            <TableRow>
+              <TableCell>번호</TableCell>
+              <TableCell>이미지</TableCell>
+              <TableCell>이름</TableCell>
+              <TableCell>생년월일</TableCell>
+              <TableCell>성별</TableCell>
+              <TableCell>직업</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {
+              customers.map(c => {
+                return (
+                  <Coustomer
+                    key={c.id}
+                    id={c.id}
+                    image={c.image}
+                    name={c.name}
+                    birthday={c.birthday}
+                    gender={c.gender}
+                    job={c.job}
+                  />
+                )
+              })
+            }
+          </TableBody>
+        </Table>
+      </Paper>
     )
   }
 }
 
-export default App;
+export default withStyles(styles)(App);
