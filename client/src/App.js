5f6f8bf0e8d59bfa898f3ebbe0f98aff7278a857
Jaeyu Jo <ehay@naver.com>
2019-07-02 03:36:58 +0900

    Add Get Customers REST API  Module

--- client/src/App.js
+++ client/src/App.js
@@ -18,34 +18,26 @@ const styles = theme => ({
   table: {
     minWidth: 1080
   }
-})
-
-const customers = [{
-  'id': 1,
-  'image': 'https://placeimg.com/64/64/1',
-  'name': '박보검',
-  'birthday': '930303',
-  'gender': '남자',
-  'job': '대학생'
-},
-{
-  'id': 2,
-  'image': 'https://placeimg.com/64/64/2',
-  'name': '김태희',
-  'birthday': '891122',
-  'gender': '여자',
-  'job': '디자이너'
-},
-{
-  'id': 3,
-  'image': 'https://placeimg.com/64/64/3',
-  'name': '홍길동',
-  'birthday': '911223',
-  'gender': '남자',
-  'job': '프로그래머'
-}]
+});
 
 class App extends React.Component {
+
+  state = {
+    customers: ""
+  }
+
+  componentDidMount() {
+    this.callApi()
+      .then(res => this.setState({customers: res}))
+      .catch(err => console.log(err));
+  }
+
+  callApi = async () => {
+    const response = await fetch('/api/customers');
+    const body = await response.json();
+    return body;
+  }
+
   render() {
     const { classes } = this.props;
     return (
@@ -63,7 +55,7 @@ class App extends React.Component {
           </TableHead>
           <TableBody>
             {
-              customers.map(c => {
+              this.state.customers ? this.state.customers.map(c => {
                 return (
                   <Coustomer
                     key={c.id}
@@ -75,7 +67,7 @@ class App extends React.Component {
                     job={c.job}
                   />
                 )
-              })
+              }) : ""
             }
           </TableBody>
         </Table>
--- server.js
+++ server.js
@@ -6,8 +6,31 @@ const port = process.env.port || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/api/hello', (req, res) => {
-    res.send({message: 'Hello Express!'});
+app.get('/api/customers', (req, res) => {
+    res.send([{
+        'id': 1,
+        'image': 'https://placeimg.com/64/64/1',
+        'name': '박보검',
+        'birthday': '930303',
+        'gender': '남자',
+        'job': '대학생'
+      },
+      {
+        'id': 2,
+        'image': 'https://placeimg.com/64/64/2',
+        'name': '김태희',
+        'birthday': '891122',
+        'gender': '여자',
+        'job': '디자이너'
+      },
+      {
+        'id': 3,
+        'image': 'https://placeimg.com/64/64/3',
+        'name': '홍길동',
+        'birthday': '911223',
+        'gender': '남자',
+        'job': '프로그래머'
+      }]);
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
